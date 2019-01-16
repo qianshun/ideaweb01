d6fdc3e5f65197906d0b53f1817a30e32528cab7
qiangsh <qianguangshun@qq.com>
2019-01-16 23:27:15 +0800

    Initial commit

---
+++ src/com/qiangsh/web/MyServlet.java
@@ -0,0 +1,18 @@
+package com.qiangsh.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       //输出一句话
+        System.out.println("这个方法执行了！");
+    }
+}
